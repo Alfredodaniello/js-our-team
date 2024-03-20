8bc21f8014facfdb1ebe9e4f358560ed7093b9d3
Alfredodaniello <alfredo_daniello@libero.it>
2024-03-20 14:35:44 +0100

    milestone 1 completed

--- js/script.js
+++ js/script.js
@@ -52,3 +52,12 @@ const myTeam = [
       },
 ]
 console.log(myTeam)
+
+//Stampo in console per ogni membro del gruppo le proprie informazioni
+
+for (let i = 0; i  < myTeam.length; i++) {
+    const member = myTeam[i];
+    console.log(member.name)
+    console.log(member.role)
+    console.log(member.photo)
+}
